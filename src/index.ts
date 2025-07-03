f24d935d382ae275cc74690d61c38796dd070205
rajeebsaraswati <rajeeb.saraswati@gmail.com>
2025-07-03 17:42:59 +0530

    vercel compatible

--- src/index.ts
+++ src/index.ts
@@ -80,23 +80,23 @@ app.use(
     }),
 );
 
-await new Promise<void>((resolve) => {
-    try {
-        httpServer.listen({ port: 4000 }, resolve);
-    } catch (error) {
-        console.error('❌ Error starting server:', error);
-        process.exit(1); // Exit process on failure
-    }
-});
-console.log(`🚀 Server ready at http://localhost:4000/`);
+// await new Promise<void>((resolve) => {
+//     try {
+//         httpServer.listen({ port: 4000 }, resolve);
+//     } catch (error) {
+//         console.error('❌ Error starting server:', error);
+//         process.exit(1); // Exit process on failure
+//     }
+// });
+// console.log(`🚀 Server ready at http://localhost:4000/`);
 
-// Handle shutdown gracefully
-process.on('SIGINT', () => {
-    console.log('🛑 Server shutting down...');
-    process.exit(0);
-});
+// // Handle shutdown gracefully
+// process.on('SIGINT', () => {
+//     console.log('🛑 Server shutting down...');
+//     process.exit(0);
+// });
 
-process.on('SIGTERM', () => {
-    console.log('🛑 Server shutting down...');
-    process.exit(0);
-});
+// process.on('SIGTERM', () => {
+//     console.log('🛑 Server shutting down...');
+//     process.exit(0);
+// });
