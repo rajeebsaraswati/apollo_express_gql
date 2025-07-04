29e0f4e9babf878ba244b06cad6d78c4df0bca9a
rajeebsaraswati <rajeeb.saraswati@gmail.com>
2025-07-04 15:17:15 +0530

    added cloude image uoload

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+import * as dotenv from "dotenv";
+dotenv.config({
+    path: path.join(process.cwd(), '.env')
+});
 import { ApolloServer } from "@apollo/server";
 import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
 import { expressMiddleware } from '@as-integrations/express5';
@@ -6,6 +10,8 @@ import http from 'http';
 import cors from 'cors';
 import path from "path";
 import multer from 'multer';
+import sharp from "sharp";
+import { v2 as cloudinary } from 'cloudinary';
 import typeDefs from './graphql/typeDefs.js';
 import resolvers from "./graphql/resolvers.js";
 import { decodeJwt } from "./utils.js";
@@ -19,24 +25,18 @@ export async function initDB() {
     });
 }
 
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 interface MyContext {
     auth?: Object;
     db: Database;
 }
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/');
-    },
-    filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
-    }
-});
-
-const upload = multer({
-    storage
-});
+const upload = multer();
 
 const app = express();
 app.use('/storage', express.static(path.join(process.cwd(), 'uploads')));
@@ -56,11 +56,31 @@ app.get('/', (req: any, res: any) => {
     return res.json({ "message": "Hello World!" })
 });
 
-app.post('/upload', upload.single('file'), (req: any, res: any) => {
+app.post('/upload', upload.single('file'), async (req: any, res: any) => {
     if (!req.file) {
         return res.status(400).json({ error: 'No file uploaded' });
     }
-    res.json({ message: 'File uploaded successfully', filename: req.file.filename });
+    // console.log(req.body.entity);
+
+    const resizeOptions = JSON.parse(req.body.resizeOptions);
+    // console.log(resizeOptions)
+
+    const resizeWidth = resizeOptions.width ?? 100;
+    const resizeHeight = resizeOptions.height ?? 100;
+    // const resizeQuality = req.body.resize.quality ?? 80;
+
+    const targetFilePath = '/tmp/' + req.file.originalname;
+
+    await sharp(req.file.buffer)
+        .resize(resizeWidth, resizeHeight)
+        .toFile(targetFilePath);
+
+    const result = await cloudinary.uploader
+        .upload(targetFilePath);
+
+    // console.log(result);
+
+    res.json({ message: 'File uploaded successfully', url: result.secure_url });
 });
 
 app.use(
@@ -82,24 +102,3 @@ app.use(
 );
 
 export default app;
-
-// await new Promise<void>((resolve) => {
-//     try {
-//         httpServer.listen({ port: 4000 }, resolve);
-//     } catch (error) {
-//         console.error('❌ Error starting server:', error);
-//         process.exit(1); // Exit process on failure
-//     }
-// });
-// console.log(`🚀 Server ready at http://localhost:4000/`);
-
-// // Handle shutdown gracefully
-// process.on('SIGINT', () => {
-//     console.log('🛑 Server shutting down...');
-//     process.exit(0);
-// });
-
-// process.on('SIGTERM', () => {
-//     console.log('🛑 Server shutting down...');
-//     process.exit(0);
-// });
